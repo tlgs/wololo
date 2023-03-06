c871732251113d1c634b01db15bd8e18128a1aac
Tiago Seabra <tlgs@users.noreply.github.com>
2023-03-06 17:46:17 +0000

    Add initial Chart.js histogram

--- scripts/render.py
+++ scripts/render.py
@@ -1,7 +1,7 @@
 import argparse
-import collections
 import json
 import statistics
+import sys
 from datetime import datetime, timezone
 from pathlib import Path
 from string import Template
@@ -10,7 +10,7 @@ from string import Template
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--input-dir", required=True)
-    # parser.add_argument("-b", "--bin", default=50)
+    parser.add_argument("--bin", default=50)
     args = parser.parse_args()
 
     ratings = []
@@ -25,25 +25,29 @@ def main():
         for player in content["leaderboard"]:
             ratings.append(player["rating"])
 
-    frequencies = collections.defaultdict(int)
+    max_rating = max(ratings)
+    frequencies = [0 for _ in range(max_rating // args.bin + 1)]
     for rating in ratings:
         frequencies[rating // args.bin] += 1
 
     p = Path(__file__).parent / "templates" / "index.html"
     with open(p) as f:
-        s = Template(f.read())
+        index_template = Template(f.read())
 
-    print(
-        s.substitute(
-            active_players=len(ratings),
-            median_rating=int(statistics.median(ratings)),
-            update_date=datetime.now(timezone.utc).isoformat(timespec="seconds"),
-        ),
-        end="",
+    template_data = dict(
+        labels=list(range(0, max_rating, args.bin)),
+        data=frequencies,
+        bin_size=args.bin,
+        active_players=len(ratings),
+        median_rating=int(statistics.median(ratings)),
+        timestamp=datetime.now(timezone.utc).isoformat(timespec="seconds"),
     )
+
+    rendered = index_template.safe_substitute(template_data)
+    sys.stdout.write(rendered)
 
     return 0
 
 
 if __name__ == "__main__":
-    raise SystemExit(main())
+    sys.exit(main())
